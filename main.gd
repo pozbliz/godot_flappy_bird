7759b54ba0a8364d4eb72853f8ea20ee2b69187e
pozbliz <sebro@hushmail.com>
2025-07-13 19:16:19 +0900

    feat: add scoring

--- main.gd
+++ main.gd
@@ -2,12 +2,13 @@ extends Node
 
 
 @export var pipe_scene: PackedScene
-@onready var score: int = 0
+var score: int
 
 func _ready() -> void:
 	$ObstacleTimer.timeout.connect(_on_obstacle_timer_timeout)
 	$Obstacles.increase_score.connect(on_obstacles_increase_score)
 	$MainMenu.new_game.connect(on_main_menu_new_game)
+	$Obstacles.player_hit.connect(on_obstacles_player_hit)
 	
 	$Player.hide()
 	$Obstacles.hide()
@@ -26,20 +27,32 @@ func new_game():
 	score = 0
 	$Player.start($PlayerStartPosition.position)
 	$HUD.show_message("Press SPACEBAR to flap through the gaps!")
+	await get_tree().create_timer(2).timeout
 	
 	$ObstacleTimer.start()
 
 func _on_obstacle_timer_timeout():
-	print("timer timeout")
 	spawn_pipes()
 	
 func spawn_pipes():
 	var pipes = pipe_scene.instantiate()
 	add_child(pipes)
+	
+	pipes.increase_score.connect(on_obstacles_increase_score)
 
 func on_obstacles_increase_score():
 	score += 1
 	$HUD.update_score(score)
+	
+func game_over():
+	$ObstacleTimer.stop()
+	$HUD.show_game_over()
+	await get_tree().create_timer(1.0).timeout
+	$HUD.hide()
+	$MainMenu.show()
+	
+func on_obstacles_player_hit():
+	game_over()
 	
 func on_main_menu_new_game():
 	new_game()
--- obstacles.gd
+++ obstacles.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Area2D
 
 
 const GAP_SIZE: int = 150
@@ -9,30 +9,33 @@ var screen_height: int
 @onready var top_collision = $TopCollision
 @onready var bottom_pipe = $BottomPipe
 @onready var bottom_collision = $BottomCollision
-@onready var enabler = $VisibleOnScreenEnabler2D
 
 signal increase_score
+signal player_hit
 
 
 func _ready() -> void:
 	screen_height = get_viewport_rect().size.y
 	
-	$VisibleOnScreenEnabler2D.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited)
-	$ScoreArea.area_entered.connect(_on_score_area_entered)
+	$ScoreArea.body_entered.connect(_on_score_body_entered)
+	self.body_entered.connect(_on_body_entered)
 
 	setup_random_pipes()
-	setup_visibility_enabler()
 
 func _process(delta: float) -> void:
 	position.x -= PIPE_SPEED * delta
+	
+	if position.x < - 200:
+		queue_free()
 
 func setup_random_pipes():
 	# Horizontal start position just outside screen
 	position.x = get_viewport_rect().size.x
-	print("pipe position x setup: ", position.x)
 	
 	# Vertical position of the random gap
 	var gap_position: float = randf_range(-100.0, 100.0)
+	
+	var score_area 
 	
 	var top_height = (screen_height / 2) + gap_position - (GAP_SIZE / 2)
 	var bottom_height = (screen_height / 2) - gap_position - (GAP_SIZE / 2)
@@ -49,23 +52,14 @@ func setup_random_pipes():
 	top_collision.position.y = top_pipe.position.y
 	bottom_collision.position.y = bottom_pipe.position.y
 	
-	$ScoreArea/CollisionShape2D.position.y = (screen_height / 2) + gap_position
+	# Position the ScoreArea in the gap
+	$ScoreArea.position.y = (screen_height / 2) + gap_position
 	
-func setup_visibility_enabler():
-	# Calculate pipe width based on actual sprite size
-	var pipe_width = top_pipe.texture.get_width() * top_pipe.scale.x
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_hit.emit()
 	
-	enabler.rect = Rect2(
-		-pipe_width/2,
-		-screen_height/2,
-		pipe_width,
-		screen_height
-	)
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	print("removing pipe")
-	queue_free()
-	
-func _on_score_area_entered():
-	increase_score.emit()
+func _on_score_body_entered(body):
+	if body.name == "Player":
+		increase_score.emit()
 	
